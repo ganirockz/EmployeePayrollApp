46c3ca06ad8973235b1b0ed32fd3cf17350538a9
ganirockz <ganeshrapeti8@gmail.com>
2020-11-23 22:08:50 +0530

    [Ganesh]Added: check for update and set values of the form element

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -1,3 +1,6 @@
+let isUpdate = false;
+let employeePayrollObj = {};
+
 class EmployeePayrollData{
 
     get id(){ return this._id; }
@@ -73,7 +76,43 @@ window.addEventListener('DOMContentLoaded',(event) => {
     salary.addEventListener('input',function(){
         output.textContent = salary.value;
     });
+
+    checkForUpdate();
 });
+
+const checkForUpdate = () => {
+    const employeePayrollJson = localStorage.getItem('editEmp');
+    isUpdate = employeePayrollJson ? true: false;
+    if(!isUpdate) return;
+    employeePayrollObj = JSON.parse(employeePayrollJson);
+    setForm(); 
+}
+
+const setForm = () => {
+    setValue('#name',employeePayrollObj._name);
+    setSelectedValues('[name=profile]',employeePayrollObj._profilePic);
+    setSelectedValues('[name=gender]',employeePayrollObj._gender);
+    setSelectedValues('[name=department]',employeePayrollObj._department);
+    setValue('#salary',employeePayrollObj._salary);
+    setValue('#notes',employeePayrollObj._note);
+    let date = StringifyDate(employeePayrollObj._startDate).split(" ");
+    setValue('#day',date[0]);
+    setValue('#month',date[1]);
+    setValue('#year',date[2]);
+}
+
+const setSelectedValues = (propertyValue, value) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        if(Array.isArray(value)){
+            if(value.includes(item.value)){
+                item.checked = true;
+            }
+        }
+        else if(item.value === value)
+            item.checked = true;
+    });
+}
 
 const save = () =>{
     try{
